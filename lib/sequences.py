785098815736aa14cb2a9a3187ff183bba837598
+Sharon <+wanjiku.kahira@gmail.com>
2024-08-30 13:48:19 +0300

    Learn sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+
+    fibonacci_list=[0,1]
+
+    while len(fibonacci_list) < length:
+
+        next_f = fibonacci_list[-1] + fibonacci_list[-2]
+
+        fibonacci_list.append(next_f)
+
+    print(fibonacci_list[:length])
+
